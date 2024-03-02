2cbf68e7e105fb33b363cdfc05809b0b84ea3fa4
nolanv45 <nolanv@udel.edu>
2024-03-02 16:33:01 -0500

    moved files into right place

--- flasktest/app.py
+++ flasktest/app.py
@@ -1,15 +1,35 @@
-import re
-from datetime import datetime
-
 from flask import render_template
 
 from flask import Flask
 
+from flask import request
+
 app = Flask(__name__)
+books = []
 
 
-@app.route("/")
-def home():
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    print(request.form)
+    books.append((
+        request.form.get()
+    ))
     return render_template(
         "searchbar.html"
+    )
+
+
+
+@app.route("/sell")
+def sellpage():
+    return render_template(
+        ""
+    )
+
+@app.route("/results")
+def results():
+    return render_template(
+        ""
     )
