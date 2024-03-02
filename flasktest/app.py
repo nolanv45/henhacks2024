7fc1961449029e8e722192b5595bae774002df67
nolanv45 <nolanv@udel.edu>
2024-03-02 17:38:29 -0500

    wokring

--- flasktest/app.py
+++ flasktest/app.py
@@ -1,18 +1,38 @@
-from flask import render_template
+from flask import render_template, Flask, url_for, render_template, request, redirect
 
-from flask import Flask
 
-from flask import request
+
+from dataclasses import dataclass
+
+@dataclass
+class Book:
+    title:str
+    course_code:str
+    condition:str
+    price:float
+    owner_email:str
+
+
 
 app = Flask(__name__)
-books = []
+
+def get_books (user_code:str, books: [Book]) -> [Book]:
+    # makes a new list of the books that correspond with the course code entered
+    new_book_list = []
+    for book in books:
+        if book.course_code == user_code:
+            new_book_list.append(book)
+    return new_book_list
+
 
 
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    print(request.form)
+    if request.method == "POST":
+        search = request.form["search_bar"]
+        return redirect(url_for("search_request", search_request=search))
     return render_template(
         "index.html"
     )
@@ -20,13 +40,9 @@ def index():
 
 
 @app.route("/sell")
-def sellpage():
-    return render_template(
-        ""
-    )
+def sellpage(search_request):
+    return f"<h1>{search_request}</h1>"
 
 @app.route("/results")
 def results():
-    return render_template(
-        ""
-    )
+    return render_template("", entries=searchedItem)
