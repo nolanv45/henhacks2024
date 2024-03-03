17701870da8ab421be8c64db058efeed28ef102c
nolanv45 <nolanv@udel.edu>
2024-03-03 11:07:32 -0500

    deleted unnecessary items

--- searchbar/script.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector(".container");
-const searchIcons = document.querySelectorAll(".search-bar i");
-
-searchIcons.forEach(searchIcon => {
-    searchIcon.addEventListener("click", () => {
-        container.classList.toggle("change");
-    });
-});
