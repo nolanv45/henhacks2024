7043e1a1312eab1adef17aaf286aaf0a6d448e95
nolanv45 <nolanv@udel.edu>
2024-03-03 11:14:49 -0500

    Changed file name

--- UDictionary/app.py
+++ UDictionary/app.py
@@ -4,14 +4,6 @@ from wtforms import StringField, SubmitField, FloatField
 import sqlite3
 from dataclasses import dataclass
 
-@dataclass
-class Book:
-    title:str
-    course_code:str
-    condition:str
-    price:float
-    owner_email:str
-
 
 #created a sellform through flask to get data from multiple fields back from html
 class SellForm(FlaskForm):
@@ -23,22 +15,9 @@ class SellForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-
-
-
+#created app in flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'theawesomekey'
-
-
-def get_books (user_code:str, books: [Book]) -> [Book]:
-    # makes a new list of the books that correspond with the course code entered
-    new_book_list = []
-    for book in books:
-        if book.course_code == user_code:
-            new_book_list.append(book)
-    return new_book_list
-
-
 
 
 #acts as the homepage
@@ -99,7 +78,7 @@ def results_helper(search):
 
 headings = ("Course Code", "Title", "Price", "Condition", "Email")
 
-
+# creates a help page
 @app.route("/buy", methods=['GET','POST'])
 def buy():
     if request.method == "POST":
@@ -108,39 +87,3 @@ def buy():
         return render_template("results.html", headings=headings, results=results)
     else:
         return render_template("buy.html")
-  
-
-
-
-            
-
-
-
-
-"""def submit_book (user_title = str, user_course_code = str, user_condition = str, user_price = float, user_owner_email = str):
-    # submits the user's information into a book that then can be appended to the overall book list
-    new_book_data = [(user_title,user_course_code,user_condition,user_price,user_owner_email)]
-    return new_book_data
-"""
-
-
-
-
-"""
-con = sqlite3.connect("books_for_sale.db",)
-cur = con.cursor()
-
-user_data = ("CISC106: Textbook", "CISC106", "New", 100.00, "ryansven@udel.edu")
-new_book_data = submit_book(*user_data)
-
-
-
-
-cur.executemany('''INSERT INTO books (title, course_code, condition, price, owner_email) VALUES (?, ?, ?, ?, ?)''', new_book_data)
-
-#cur.executemany('''DELETE FROM books (title, course_code, condition, price, owener_email) WHERE (?, ?, ?, ?, ?)''', removing_book_data)
-
-con.commit()
-
-
-"""
